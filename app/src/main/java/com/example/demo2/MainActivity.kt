8af2fea7785fb54498aec1105434a56988212d23
ZdwCode <276448724@qq>
2020-08-02 16:52:33 +0800

    1.FrameLayou，LinearLayout，RelativeLayout

---
+++ app/src/main/java/com/example/demo2/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.demo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /**
+         * 1.xml配置 layout目录下创建xml文件 进行界面布局
+         * 2.code创建  用代码的方式船舰布局和添加控件
+         */
+    }
+}
